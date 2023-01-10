5a54be1584b68382e2cc267745b14ddc6e4d64c3
RogelioElMero <rogelio_1322134109@uptecamac.edu.mx>
2023-01-09 20:39:30 -0600

    first commit

---
+++ prueba.js
@@ -0,0 +1,43 @@
+
+const fechaAho= new Date();
+
+
+
+var persona={
+	nombre:null,
+	apellido:null,
+	FechaNacimiento:null,
+	edad:null,
+
+	years(){
+		this.edad= fechaAho.getFullYear() - this.FechaNacimiento.substring(0,4);
+		
+		if(this.FechaNacimiento.substring(5,7)>fechaAho.getDay()){
+			this.edad=this.edad-1;
+
+		}else if(this.FechaNacimiento.substring(5,7)==fechaAho.getDay()){
+
+			if(this.FechaNacimiento.substring(8,10)>fechaAho.getDate()-1){
+				this.edad=this.edad-1;
+			}
+		
+		}else{
+			this.edad= fechaAho.getFullYear() - this.FechaNacimiento.substring(0,4);
+		}
+	}
+}
+
+
+function Save(){
+persona.nombre=document.getElementById("name").value;
+persona.apellido=document.getElementById("ap").value;
+persona.FechaNacimiento=document.getElementById("FechaNac").value;
+
+persona.years();
+alert("guardado exitosamente");
+
+}
+
+function mostrar(){
+	alert("buenas tardes, mi nombre es "+persona.nombre+" "+nombre.apellido+" y mi edad es de "+persona.edad+" años :D");
+}
