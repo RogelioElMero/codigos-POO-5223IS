3ce416f9f68c7a229d0d50ef368e349fad0e952d
RogelioElMero <rogelio_1322134109@uptecamac.edu.mx>
2023-01-15 17:11:00 -0600

    inplementacion de la plantilla con clase Persona

---
+++ plantilla.js
@@ -0,0 +1,19 @@
+class Persona{
+     nombre;
+     Apellidos;
+     f_Nacimiento;
+     #Curp;
+     #Rfc;
+
+     mostrarRfc=function(){
+
+     }
+
+     genero=function(){
+
+     }
+
+     edad=function(){
+        
+     }
+}
